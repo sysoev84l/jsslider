9eea57c173e9d3be235e82d7774d9d25b9822b19
sysoev84l <sysoev84l@ya.ru>
2023-06-15 08:23:04 +0300

    jsslider

--- start/bs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#browser-sync start --server --files "*.css,assets/js/*.js,assets/scss/*.scss,assets/css/*.css,*.html,*.*"
-browser-sync https://jsslider.ddns.net --files "*.css,assets/js/*.js,assets/scss/*.scss,assets/css/*.css,*.html,*.*"
--- start/css.sh
+++
@@ -1,2 +0,0 @@
-notify-send "start css preprocessor for $BASENAME" -i "css3.svg"
-sass --watch ./assets/scss/style.scss:./assets/css/style.css
