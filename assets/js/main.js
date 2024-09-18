cb90cf0b0c5dcf59b878f36e1bd8419db0bd1424
sysoev84l <sysoev84l@ya.ru>
2024-09-18 19:57:52 +0300

    jsslider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-let hostName = document.location.origin;
+let hostName = document.location.href;
 let hostNameLink = document.querySelector('.host-name');
 hostNameLink.innerHTML = hostName;
+hostNameLink.href = hostName;
 const slider = new Slider;
 slider.start('itk-slider', arr_png);
